1c091702d823b2790e4d93fb3b2b1c36bbd2ff5c
Anthony Lam <anthony.lam.ags@gmail.com>
2016-07-08 12:02:55 +0800

    separated local and prod logs retrieval

--- ssl-connection/local-es.py
+++ ssl-connection/local-es.py
@@ -3,22 +3,22 @@ from elasticsearch import RequestsHttpConnection
 from elasticsearch.client import Elasticsearch
 from elasticsearch.exceptions import ElasticsearchException
 
-ES_IP = '172.30.151.164'
+ES_IP = '192.168.33.100'
 
 def new_elasticsearch():
 
     try:
         es = Elasticsearch(host=ES_IP,
                               port=9200,
-                              use_ssl=True,
+                              use_ssl=False,
                               connection_class=RequestsHttpConnection,
                               timeout=10,
-                              ca_certs='/opt/secrets/ea.ca',
-                              client_cert='/opt/secrets/ea.cert',
-                              client_key='/opt/secrets/ea.key',
+                              # ca_certs='/opt/secrets/ea.ca',
+                              # client_cert='/opt/secrets/ea.cert',
+                              # client_key='/opt/secrets/ea.key',
                               )
 
-        logs = es.search(index='api-factory-asia-qa*', q='_source_include=%40timestamp%2C%2A&ignore_unavailable=true&size=10000')
+        logs = es.search(index='logstash-*', q='_source_include=%40timestamp%2C%2A&ignore_unavailable=true&size=10000')
         print logs
     except ElasticsearchException as e:
         print "\n\n\n[ElasticsearchError]", e
